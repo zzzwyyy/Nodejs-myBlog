ea4b9d6e4f23fae80fee4085ce5b32a1f4a82a68
wyy <610193653@qq.com>
2019-10-24 22:48:57 +0800

    router test

---
+++ blog-1/src/router/blog.js
@@ -0,0 +1,52 @@
+const {
+    getList,
+    getDetail,
+    newBlog,
+    updateBlog,
+    delBlog
+} = require('../controller/blog')
+const { SuccessModel, ErrorModel } = require('../Model/resModel.js')
+
+const handleBlogRouter = (req, res) => {
+    const method = req.method // GET POST
+    const id = req.query.id
+
+    
+    // 获取博客列表
+    if (method === 'GET' && req.path === '/api/blog/list') {
+        const author = req.query.author || ''
+        const keyword = req.query.weyword || ''
+        const listData = getList(author, keyword)
+        return new SuccessModel(listData)
+    }
+     // 获取博客详情
+     if (method === 'GET' && req.path === '/api/blog/detail') {
+         const data = getData(id)
+         return new SuccessModel(data) 
+     }
+      // 新建一篇博客
+    if (method === 'POST' && req.path === '/api/blog/new') {
+        const data = newBlog(req.body)
+        return new SuccessModel(data)
+    }
+    // 更新一篇博客
+    if (method === 'POST' && req.path === '/api/blog/update') {
+        const result = updateBlog(id, req.body)
+        if(result) {
+            return new SuccessModel()
+        } else {
+            return new ErrorModel('更新博客失败')
+        }
+    }
+    // 删除一篇博客
+    if (method === 'POST' && req.path === '/api/blog/del') {
+        const result = delBlog(id)
+        if(result) {
+            return new SuccessModel()
+        } else {
+            return new ErrorModel('删除博客失败')
+        }
+    }
+}
+
+module.exports = handleBlogRouter
---
+++ blog-1/src/router/user.js
@@ -0,0 +1,18 @@
+const { SuccessModel, ErrorModel } = require('../model/resModel')
+const loginCheck = require('../controller/user')
+
+const handleUserRouter = (req, res) => {
+    const method = req.method // GET POST
+
+    // 登录
+    if (method === 'POST' && req.path === '/api/user/login') {
+        const { username, password } = req.body
+        const result = loginCheck(username, password)
+        if(result) {
+            return new SuccessModel()
+        } 
+        return new ErrorModel('登陆失败')
+    }
+}
+
+module.exports = handleUserRouter
